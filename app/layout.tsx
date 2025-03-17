6c956565134a00db2ed684b61dad8633abad7e38
spoorthi <spoorthiachar9@gmail.com>
2025-03-17 20:41:03 +0530

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Tata Technologies - Engineering a better world",
+  description:
+    "We use our deep manufacturing knowledge and expertise in turnkey product development to help our customers develop products that are safe, sustainable, and deliver great customer experience.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
+
